fdee57542f29e5192d7c90e46d778ce466be5604
wesselrijk <wesselrijk@gmail.com>
2019-05-06 10:27:42 +0200

    Latest MAC push, all operations work except back button. Layout needs to be updated further

--- app/src/main/java/com/example/madlibs/MadLib.java
+++ app/src/main/java/com/example/madlibs/MadLib.java
@@ -3,12 +3,10 @@ package com.example.madlibs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 import java.io.InputStream;
 
